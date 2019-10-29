ac4dc5a67c948215778969bcfc0ae439ff135813
Maria Eduarda Kawakami <kawakamimadu@gmail.com>
2019-10-29 13:42:47 -0300

    Update world.h
    Feat: world

--- Classes/world.h
+++ Classes/world.h
@@ -1,12 +1,15 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+#include "position.h"
+#include "reference.h"
 #include "entity.h"
 
 class World{
   public:
     World(int m, int n);
     void random_world(void);
+    Entity *get_entity(Position *curr);
 
   private:
     vector<vector <Entity*> > map;
